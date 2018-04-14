dd2d0a154b639ef0e84b4912a59b5ae173509e41
nknezek <nknezek@gmail.com>
2018-04-13 17:59:00 -0700

    working nature run script

--- control_scripts/run_for_nature.py
+++ control_scripts/run_for_nature.py
@@ -14,7 +14,7 @@ print(sys.argv)
 layer_thickness = 100 # m
 overturn = 600 # Myr
 
-times = np.linspace(0,4568e6*365.25*24*3600,60000)
+times = np.linspace(0,4568e6*365.25*24*3600,20000)
 
 ## background mantle state
 fraction_MgFe_b = 0.8
@@ -33,6 +33,7 @@ X_Mgs = np.linspace(1e-5,.05,round(.05/.005)+1)
 X_Sis = np.linspace(1e-5,.05,round(.05/.005)+1)
 X_Os = np.linspace(1e-5,.15,round(.15/.005)+1)
 
+basefolder = '../computed_solutions_nature/'
 for T_cmb0 in T_cmbs:
     for X_Mg_0 in X_Mgs:
         for X_Si_0 in X_Sis:
@@ -58,12 +59,13 @@ for T_cmb0 in T_cmbs:
 
                 try :
                     solution = pl.integrate(times, x0)
-                    filepath = '../computed_solutions_new/Tc{:d}_dT{:d}_XM{:.2f}_XS{:.2f}_XO{:.2f}_fMb{:.2f}_Xmb{:.2f}_XSb{:.2f}_nu{:.0e}_lthck{:.0e}_ovt{:.0e}/'.format(T_cmb0, deltaT0,
-                            X_Mg_0, X_Si_0, X_O_0, fraction_MgFe_b, X_MgFeO_b, X_SiO2_b, nu_present,layer_thickness,overturn)
-                    if not os.path.exists('../computed_solutions_new/'):
-                        os.mkdir('../computed_solutions_new/')
+                    filepath = basefolder+ "Tc{:.1f}_XM{:.3f}_XS{:.3f}_XO{:.3f}/".format(T_cmb0, X_Mg_0, X_Si_0, X_O_0)
+                    if not os.path.exists(basefolder):
+                        os.mkdir(basefolder)
+                        print(basefolder)
                     if not os.path.exists(filepath):
                         os.mkdir(filepath)
+                        print(filepath)
                     dill.dump((pl,times,solution), open(filepath+'data.m','wb'))
                     mplt.temperature(pl, times, solution, filepath=filepath)
                     mplt.coremoles(pl, times, solution, filepath=filepath)
@@ -71,19 +73,20 @@ for T_cmb0 in T_cmbs:
                     plt.close('all')
                     time = str(datetime.datetime.now())
                     r_i = pl.core_layer.r_i(solution[-1,0], one_off=True)
-                    del pl
                     csvdata = [time, T_cmb0, deltaT0, r_i, X_Mg_0, X_Si_0, X_O_0, fraction_MgFe_b, X_MgFeO_b, X_SiO2_b, nu_present,layer_thickness,overturn]
-                    with open(r'../computed_solutions_new/run_data{}.csv'.format(iT), 'a') as f:
+                    print(csvdata)
+                    with open(basefolder+'run_data{}.csv'.format(iT), 'a') as f:
                         writer = csv.writer(f)
                         writer.writerow(csvdata)
                     f.close()
-                    copyfile('./dynamo_power.py',filepath+'dynamo_power.py')
+                    del pl
+
                 except :
                     del pl
                     time = str(datetime.datetime.now())
                     r_i = np.nan
                     csvdata = [time, T_cmb0, deltaT0, r_i, X_Mg_0, X_Si_0, X_O_0, fraction_MgFe_b, X_MgFeO_b, X_SiO2_b, nu_present,layer_thickness,overturn]
-                    with open(r'../computed_solutions_nature/run_data{}.csv'.format(iT), 'a') as f:
+                    with open(basefolder+'run_data{}.csv'.format(iT), 'a') as f:
                         writer = csv.writer(f)
                         writer.writerow(csvdata)
                     f.close()
