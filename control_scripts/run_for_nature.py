d433d8c03a46f3fd51b52d89f6c61bf1bc38839b
nknezek <nknezek@gmail.com>
2018-04-16 17:46:03 -0700

    modified run nature script

--- control_scripts/run_for_nature.py
+++ control_scripts/run_for_nature.py
@@ -36,61 +36,67 @@ X_Os = np.linspace(1e-5,.15,round(.15/.005)+1)
 
 basefolder = '../computed_solutions_nature/'
 for T_cmb0 in T_cmbs:
-    for X_Mg_0 in X_Mgs:
-        for X_Si_0 in X_Sis:
-            for X_O_0 in X_Os:
-                print(T_cmb0, X_Si_0, X_Mg_0, X_O_0)
-                pl = mg_si.planet.Custom()
-                try:
-                    pl.reactions._set_layer_thickness(layer_thickness)
-                    pl.reactions._set_overturn_time(overturn)
-                    deltaT0 = pl.mantle_layer.get_dT0(T_cmb0)
-                    T_um0 = T_cmb0-deltaT0
-                    Moles_0 = pl.reactions.compute_Moles_0(X_Mg_0, X_Si_0, X_O_0, T_cmb0)
-                    x0 = [T_cmb0, T_um0]
-                    x0 = x0+Moles_0
-                    pl.params.reactions.Moles_0 = Moles_0
+	for X_Mg_0 in X_Mgs:
+		for X_Si_0 in X_Sis:
+			for X_O_0 in X_Os:
+				print(T_cmb0, X_Si_0, X_Mg_0, X_O_0)
+				pl = mg_si.planet.Custom()
+				pl.reactions._set_layer_thickness(layer_thickness)
+				pl.reactions._set_overturn_time(overturn)
+				deltaT0 = pl.mantle_layer.get_dT0(T_cmb0)
+				T_um0 = T_cmb0-deltaT0
+				try:
+					filepath = basefolder+ "Tc{:.1f}_XM{:.3f}_XS{:.3f}_XO{:.3f}/".format(T_cmb0, X_Mg_0, X_Si_0, X_O_0)
+					if not os.path.exists(basefolder):
+						os.mkdir(basefolder)
+					if os.path.exists(filepath):
+						print(filepath+' already computed')
+						continue
+					if not os.path.exists(filepath):
+						os.mkdir(filepath)
 
-                    Mm_b = pl.reactions.mantle.compute_Mm_b(X_MgFeO=X_MgFeO_b, X_SiO2=X_SiO2_b, MgNumFp=MgNumFp, MgNumPv=MgNumPv)
-                    pl.params.reactions.Mm_b = Mm_b
+					Moles_0 = pl.reactions.compute_Moles_0(X_Mg_0, X_Si_0, X_O_0, T_cmb0)
+					x0 = [T_cmb0, T_um0]
+					x0 = x0+Moles_0
+					pl.params.reactions.Moles_0 = Moles_0
 
-                    T_present = 1350 # [K]
-                    nu_old =  nu_present/1e3
-                    T_old = T_um0
-                    A,nu0 = pl.mantle_layer.find_arrenhius_params(nu_present, T_present, nu_old, T_old, set_values=True)
+					Mm_b = pl.reactions.mantle.compute_Mm_b(X_MgFeO=X_MgFeO_b, X_SiO2=X_SiO2_b, MgNumFp=MgNumFp, MgNumPv=MgNumPv)
+					pl.params.reactions.Mm_b = Mm_b
 
-                    solution = pl.integrate(times, x0)
-                    filepath = basefolder+ "Tc{:.1f}_XM{:.3f}_XS{:.3f}_XO{:.3f}/".format(T_cmb0, X_Mg_0, X_Si_0, X_O_0)
-                    if not os.path.exists(basefolder):
-                        os.mkdir(basefolder)
-                        print(basefolder)
-                    if not os.path.exists(filepath):
-                        os.mkdir(filepath)
-                        print(filepath)
-                    dill.dump((pl,times,solution), open(filepath+'data.m','wb'))
-                    mplt.temperature(pl, times, solution, filepath=filepath)
-                    mplt.coremoles(pl, times, solution, filepath=filepath)
-                    mplt.composition(pl, times, solution, filepath=filepath)
-                    plt.close('all')
-                    time = str(datetime.datetime.now())
-                    r_i = pl.core_layer.r_i(solution[-1,0], one_off=True)
-                    csvdata = [time, r_i, T_cmb0, X_Mg_0, X_Si_0, X_O_0, MgNumFp, MgNumPv, X_MgFeO_b, X_SiO2_b, nu_present, deltaT0, layer_thickness, overturn]
-                    print(csvdata)
-                    with open(basefolder+'run_data{}.csv'.format(iT), 'a') as f:
-                        writer = csv.writer(f)
-                        writer.writerow(csvdata)
-                    f.close()
-                    del pl
+					T_present = 1350 # [K]
+					nu_old =  nu_present/1e3
+					T_old = T_um0
+					A,nu0 = pl.mantle_layer.find_arrenhius_params(nu_present, T_present, nu_old, T_old, set_values=True)
 
-                except :
-                    del pl
-                    time = str(datetime.datetime.now())
-                    r_i = np.nan
-                    csvdata = [time, r_i, T_cmb0, X_Mg_0, X_Si_0, X_O_0, MgNumFp, MgNumPv, X_MgFeO_b, X_SiO2_b, nu_present, deltaT0, layer_thickness, overturn]
-                    with open(basefolder+'run_data{}.csv'.format(iT), 'a') as f:
-                        writer = csv.writer(f)
-                        writer.writerow(csvdata)
-                    f.close()
-                del csvdata,writer,f
+					solution = pl.integrate(times, x0)
+					print(filepath)
+					dill.dump((pl,times,solution), open(filepath+'data.m','wb'))
+					mplt.temperature(pl, times, solution, filepath=filepath)
+					mplt.coremoles(pl, times, solution, filepath=filepath)
+					mplt.composition(pl, times, solution, filepath=filepath)
+					plt.close('all')
+					time = str(datetime.datetime.now())
+					r_i = pl.core_layer.r_i(solution[-1,0], one_off=True)
+					csvdata = [time, r_i, T_cmb0, X_Mg_0, X_Si_0, X_O_0, MgNumFp, MgNumPv, X_MgFeO_b, X_SiO2_b, nu_present, deltaT0, layer_thickness, overturn]
+					print(csvdata)
+					with open(basefolder+'run_data{}.csv'.format(iT), 'a') as f:
+						writer = csv.writer(f)
+						writer.writerow(csvdata)
+					f.close()
+					del pl
+					del csvdata,writer,f
+				except:
+					try:
+						del pl
+						time = str(datetime.datetime.now())
+						r_i = np.nan
+						csvdata = [time, r_i, T_cmb0, X_Mg_0, X_Si_0, X_O_0, MgNumFp, MgNumPv, X_MgFeO_b, X_SiO2_b, nu_present, deltaT0, layer_thickness, overturn]
+						with open(basefolder+'run_data{}.csv'.format(iT), 'a') as f:
+							writer = csv.writer(f)
+							writer.writerow(csvdata)
+						f.close()
+						del csvdata,writer,f
+					except:
+						print("couldn't do anything")
 
 
